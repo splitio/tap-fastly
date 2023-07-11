d536f79a1c091ed7fcbff15d35730dbcb821f889
Pierre-Alexandre Masse <pa.masse@split.io>
2023-07-11 04:33:39 +0000

    adjusted setup to include properly the schemas

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
       name="tap-fastly",
       version="0.1.0",
       description="Singer.io tap for extracting Fastly billing data.",
-      author="Stitch",
-      url="http://singer.io",
+      author="SplitIO",
+      url="http://split.io",
       classifiers=["Programming Language :: Python :: 3 :: Only"],
       py_modules=["tap_fastly"],
       install_requires=[
@@ -15,12 +15,13 @@ setup(
             "pendulum"
       ],
       entry_points="""
-      [console_scripts]
-      tap-fastly=tap_fastly:main
+            [console_scripts]
+            tap-fastly=tap_fastly:main
       """,
-      packages=["tap_fastly"],
-      package_data = {
-            "schemas": ["tap_fastly/schemas/*.json"]
-      },
-      include_package_data=True,
+      packages=find_packages(),
+      package_data={
+          'tap_fastly': [
+              'schemas/*.json'
+          ]
+      }
 )
